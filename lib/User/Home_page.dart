314b8ebaa58914475d7d2693b83beac845ae396d
Akshat <a_khandelwal@ch.iitr.ac.in>
2024-04-11 00:10:17 +0530

    home page update

--- lib/User/Home_page.dart
+++ lib/User/Home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -6,24 +7,37 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Krashi'),
+        automaticallyImplyLeading: false, // Remove the back button icon
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // Add profile icon onPressed logic
+            },
+          ),
+        ],
       ),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.fromLTRB(0,20,0,0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 20.0),
             Expanded(
               child: ListView.builder(
+                padding: EdgeInsets.all(20),
                 itemCount: 10,
                 itemBuilder: (context, index) {
                   return Container(
@@ -40,22 +54,128 @@ class HomePage extends StatelessWidget {
                         ),
                       ],
                     ),
-                    child: ListTile(
-                      title: Text('Item $index'),
-                      subtitle: Text('Description of item $index'),
-                      leading: CircleAvatar(
-                        child: Text((index + 1).toString()),
-                      ),
-                      onTap: () {
-                        // Add item tap logic
-                      },
-                    ),
+                    child: ProductItem()
                   );
                 },
               ),
             ),
+            SizedBox(height: 20.0),
+            SizedBox(
+              height: 50.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // View cart logic
+                    },
+                    child: Text('View Cart'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Wishlist logic
+                    },
+                    child: Text('Wishlist'),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+class ProductItem extends StatefulWidget {
+  @override
+  _ProductItemState createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  int quantity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // margin: EdgeInsets.symmetric(vertical: 10.0),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                height: 250.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/product_image.jpg'), // Replace with your image
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                onPressed: () {
+                  // Add to wishlist logic
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        if (quantity > 1) {
+                          quantity--;
+                        }
+                      });
+                    },
+                  ),
+                  Text('$quantity'),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        quantity++;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add to cart logic
+                },
+                child: Text('Add to Cart'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
