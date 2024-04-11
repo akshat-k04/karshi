13018b70e3a56577fd3d171393b0d10fd0d8d61b
Ishan Singh <Is26092003@gmail.com>
2024-04-11 23:35:33 +0530

    dashboard

---
+++ lib/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyAppColors {
+  static const Color textColor =
+      Color.fromRGBO(14, 197, 93, 100); // Custom green color
+  static const Color backgroundColor =
+      Color.fromRGBO(0, 27, 21, 100); // Custom green color
+  // Custom green color
+  static const Color bgGreen = Color.fromRGBO(0, 37, 28, 100);
+  static const Color selectedGreen =
+      Color.fromRGBO(24, 105, 72, 100); // Custom green color
+}
