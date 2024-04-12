cc56c0a2a74588c8c1f4ca664485aadb1a7fa0dd
Ishan Singh <Is26092003@gmail.com>
2024-04-12 20:17:24 +0530

    blabla

--- lib/custom widgets/loading_page.dart
+++ lib/custom widgets/loading_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class LoadingPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CircularProgressIndicator(), // Or any other loading indicator
+            Image.asset('assets/images/loading.gif'), // Use your asset path
             SizedBox(height: 20.0),
             Text('Loading...'),
           ],
